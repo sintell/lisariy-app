f28258b3da7f8d66624755a4288205626d083a31
sintell <a.anatskiy@hh.ru>
2019-03-09 06:08:36 +0300

    add caching and some perfomance tweeks

--- app/static/src/App.vue
+++ app/static/src/App.vue
@@ -7,10 +7,10 @@
       position="top-right"
     />
     <vk-navbar>
+      <vk-navbar-logo slot="center">
+        <img class="nav-logo-icon" src="/fox.svg" alt="Lisariy fox icon"/>
+      </vk-navbar-logo>
       <vk-navbar-nav slot="center">
-        <vk-navbar-logo>
-          <img class="nav-logo-icon" src="/fox.svg" alt="Lisariy fox icon"/>
-        </vk-navbar-logo>
         <vk-navbar-nav-item
           :active="this.$route.path === '/'"
           href="/"
@@ -46,9 +46,9 @@
     <hr class="uk-divider-icon"/>
     <div class="uk-container">
       <div class="footer">
-        <div>Site created by Antky from <a href="https://aaleks.ru">antky.ru</a></div>
+        <div>Site created by Antky from <a href="https://aaleks.ru" rel="noreferrer">antky.ru</a></div>
         <div>
-          Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+          Icons made by <a href="https://www.freepik.com/" title="Freepik" rel="noreferrer">Freepik</a> from <a href="https://www.flaticon.com/" rel="noreferrer" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" rel="noreferrer" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
         </div>
         <div>Copyright 2019 Lisariy</div>
       </div>
